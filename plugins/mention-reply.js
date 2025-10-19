6dbe4b189a4602e0935c6a7df9689a09bd898b82
FAZI-MD <121059808+Faizan-MD-BOTZ@users.noreply.github.com>
2025-10-20 01:18:13 +0500

    Update mention-reply.js

--- plugins/mention-reply.js
+++ plugins/mention-reply.js
@@ -40,12 +40,12 @@ cmd({
           forwardingScore: 999,
           isForwarded: true,
           externalAdReply: {
-            title: config.BOT_NAME || "ANAYAT-AI 🥀",
-            body: config.DESCRIPTION || "𝐀𝐍𝐀𝐘𝐀𝐓-𝐀𝐈 🤌💗",
+            title: config.BOT_NAME || "FAIZAN-AI 🥀",
+            body: config.DESCRIPTION || "FAIZAN-AI 🤌💗",
             mediaType: 1,
             renderLargerThumbnail: true,
             thumbnail: thumbnailBuffer,
-            mediaUrl: "https://files.catbox.moe/r2ncqh", // Static image URL
+            mediaUrl: "https://files.catbox.moe/jq65ev.jpg", // Static image URL
             sourceUrl: "https://wa.me/message/VRZ5QLDAHXKSF1",
             showAdAttribution: true
           }
