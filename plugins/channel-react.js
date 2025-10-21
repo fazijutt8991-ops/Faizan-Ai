15783cce272f6b5073ad828e95788464a9640043
FAZI-MD <121059808+Faizan-MD-BOTZ@users.noreply.github.com>
2025-10-21 21:44:05 +0500

    Update channel-react.js

--- plugins/channel-react.js
+++ plugins/channel-react.js
@@ -45,7 +45,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const channelMeta = await conn.newsletterMetadata("invite", channelId);
         await conn.newsletterReactMessage(channelMeta.id, messageId, emoji);
 
-        return reply(`╭━━━〔 *ANAYAT-AI* 〕━━━┈⊷
+        return reply(`╭━━━〔 *𝐹𝑎𝑖𝑧𝑎𝑛-𝐴𝑖* 〕━━━┈⊷
 ┃▸ *Success!* Reaction sent
 ┃▸ *Channel:* ${channelMeta.name}
 ┃▸ *Reaction:* ${emoji}
