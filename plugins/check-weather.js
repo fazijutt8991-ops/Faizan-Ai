5f67ee4878026f4ca85a23490b56030b2f8f0ca8
FAZI-MD <121059808+Faizan-MD-BOTZ@users.noreply.github.com>
2025-10-21 21:35:22 +0500

    Update check-weather.js

--- plugins/check-weather.js
+++ plugins/check-weather.js
@@ -28,7 +28,7 @@ async (conn, mek, m, { from, q, reply }) => {
 > 💨 *Wind Speed*: ${data.wind.speed} m/s
 > 🔽 *Pressure*: ${data.main.pressure} hPa
 
-> *𝐀𝐍𝐀𝐘𝐀𝐓-𝐀𝐈*
+> *𝐹𝑎𝑖𝑧𝑎𝑛-𝐴𝑖*
 `;
         return reply(weather);
     } catch (e) {
