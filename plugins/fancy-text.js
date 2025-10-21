b3f5d6ebe33f2d8721cc4e7d90eb19dd8c776b36
FAZI-MD <121059808+Faizan-MD-BOTZ@users.noreply.github.com>
2025-10-22 01:19:15 +0500

    Update fancy-text.js

--- plugins/fancy-text.js
+++ plugins/fancy-text.js
@@ -22,7 +22,7 @@ cmd({
     }
 
     const fonts = response.data.result.map(item => `*${item.name}:*\n${item.result}`).join("\n\n");
-    const resultText = `✨ *Fancy Fonts Converter* ✨\n\n${fonts}\n\n> *𝐀𝐍𝐀𝐘𝐀𝐓-𝐀𝐈*`;
+    const resultText = `✨ *Fancy Fonts Converter* ✨\n\n${fonts}\n\n> _FAIZAN-AI🫰_`;
 
     await conn.sendMessage(from, { text: resultText }, { quoted: m });
   } catch (error) {
