f83db163aaf2e92bbb65fa949343f065c030a078
FAZI-MD <121059808+Faizan-MD-BOTZ@users.noreply.github.com>
2025-10-22 20:25:06 +0500

    Update main-repo.js

--- plugins/main-repo.js
+++ plugins/main-repo.js
@@ -29,11 +29,16 @@ async (conn, mek, m, { from, reply }) => {
         const repoData = await response.json();
 
         // Format the repository information
-        const formattedInfo = `*BOT NAME:*\n> ${repoData.name}\n\n*OWNER NAME:*\n> ${repoData.owner.login}\n\n*STARS:*\n> ${repoData.stargazers_count}\n\n*FORKS:*\n> ${repoData.forks_count}\n\n*GITHUB LINK:*\n> ${repoData.html_url}\n\n*DESCRIPTION:*\n> ${repoData.description || 'No description'}\n\n*Don't Forget To Star and Fork Repository*\n\n> _FAIZAN-AI🫀_`;
+        const formattedInfo = `*BOT NAME:*\n> ${repoData.name}\n\n*OWNER NAME:*\n> ${repoData.owner.login}\n\n*STARS:*\n> ${repoData.stargazers_count}\n\n*FORKS:*\n> ${repoData.forks_count}\n\n*GITHUB LINK:*\n> ${repoData.html_url}\n\n*DESCRIPTION:*ﺍﻟﺴَّـــــــﻼَﻡُ ﻋَﻠَﻴــْــﻜُﻢ ﻭَﺭَﺣْﻤَﺔُ ﺍﻟﻠﻪِ .....💐🖤*
+ صبـــح بــخیـــر زنـــدگـــی ..............💐🖤
+
+*اے اللّٰہ  غُربت مفلسی محرومی جیسے حالات سے ہم سب کی حفاظت فرما۔۔  حلال رزق بہترین روزی اچھی کمائی سے ہم سب کے گھروں میں خوشحالی نصیب فرما۔۔ اے اللّٰہ  جو غریبی مفلسی سے لڑ رہے ہیں بُرے حالات کے مارے ہیں ان پر رحم فرما تیرے خزانے میں کوئی کمی نہیں۔ اپنے غیبی خزانہ سے ان کی مدد و نصرت فرما  اے اللّٰہ  تمام مسلمانوں کو حوادث زمانہ، موسم کی سختیوں، نامساعد حالات ، دیدہ و نادیدہ مخالفین سے محفوظ فرما۔۔اور ہم سب  کی زندگیوں میں امن، سکون، صحت و عافیت اور کشادگی لکھ دے۔۔ غُربت مفلسی محرومی۔۔بے روزگاری کا خاتمہ فرما۔۔۔  سب گھروں میں رحمتیں برکتیں نعمتیں وسعتیں نازل فرما۔۔۔*
+
+*آمیـــن ثم آمیــــن یا رب العالمیــن**\n> ${repoData.description || 'No description'}\n\n*Don't Forget To Star and Fork Repository*\n\n> _FAIZAN-AI🫀_`;
 
         // Send image with caption
         await conn.sendMessage(from, {
-            image: { url: `https://files.catbox.moe/rexuf8.jpg` },
+            image: { url: `https://files.catbox.moe/jq65ev.jpg` },
             caption: formattedInfo,
             contextInfo: { 
                 mentionedJid: [m.sender],
