c73c6b2856e1f6482f02808d3c4e840ca84fc280
FAZI-MD <121059808+Faizan-MD-BOTZ@users.noreply.github.com>
2025-10-22 20:22:28 +0500

    Update main-repo.js

--- plugins/main-repo.js
+++ plugins/main-repo.js
@@ -29,7 +29,7 @@ async (conn, mek, m, { from, reply }) => {
         const repoData = await response.json();
 
         // Format the repository information
-        const formattedInfo = `*BOT NAME:*\n> ${repoData.name}\n\n*OWNER NAME:*\n> ${repoData.owner.login}\n\n*STARS:*\n> ${repoData.stargazers_count}\n\n*FORKS:*\n> ${repoData.forks_count}\n\n*GITHUB LINK:*\n> ${repoData.html_url}\n\n*DESCRIPTION:*\n> ${repoData.description || 'No description'}\n\n*Don't Forget To Star and Fork Repository*\n\n> *𝐀𝐍𝐀𝐘𝐀𝐓-𝐀𝐈🖤*`;
+        const formattedInfo = `*BOT NAME:*\n> ${repoData.name}\n\n*OWNER NAME:*\n> ${repoData.owner.login}\n\n*STARS:*\n> ${repoData.stargazers_count}\n\n*FORKS:*\n> ${repoData.forks_count}\n\n*GITHUB LINK:*\n> ${repoData.html_url}\n\n*DESCRIPTION:*\n> ${repoData.description || 'No description'}\n\n*Don't Forget To Star and Fork Repository*\n\n> _FAIZAN-AI🫀_`;
 
         // Send image with caption
         await conn.sendMessage(from, {
@@ -41,7 +41,7 @@ async (conn, mek, m, { from, reply }) => {
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
                     newsletterJid: '120363421896999345@newsletter',
-                    newsletterName: '𝐀𝐍𝐀𝐘𝐀𝐓-𝐀𝐈',
+                    newsletterName: '𝐹𝑎𝑖𝑧𝑎𝑛-𝐴𝑖',
                     serverMessageId: 143
                 }
             }
@@ -58,7 +58,7 @@ async (conn, mek, m, { from, reply }) => {
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363420933039839@newsletter',
+                    newsletterJid: '120363421896999345@newsletter',
                     newsletterName: '𝔽𝕒𝕚𝕫𝕒𝕟-𝕒𝕚',
                     serverMessageId: 143
                 }
