03de2cd7a4d88cfa56203d5fc1d7ccd8b7b258ab
FAZI-MD <121059808+Faizan-MD-BOTZ@users.noreply.github.com>
2025-10-22 21:11:31 +0500

    Update yt-stalk.js

--- plugins/yt-stalk.js
+++ plugins/yt-stalk.js
@@ -32,7 +32,7 @@ cmd({
       + `┃🎥 *Videos:* ${yt.video_count}\n`
       + `┃🔗 *Channel Link:* (${yt.channel})\n`
       + `╰━━━⪼\n\n`
-      + `🔹 *𝐀𝐍𝐀𝐘𝐀𝐓-𝐀𝐈*`;
+      + `🔹 *_FAIZAN-AI_*`;
 
     await conn.sendMessage(from, {
       image: { url: yt.avatar },
