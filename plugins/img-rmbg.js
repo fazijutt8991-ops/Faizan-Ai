272d0f99093df8251d6a2e288ade359a8c3f6e72
FAZI-MD <121059808+Faizan-MD-BOTZ@users.noreply.github.com>
2025-10-25 20:58:25 +0500

    Update img-rmbg.js

--- plugins/img-rmbg.js
+++ plugins/img-rmbg.js
@@ -75,7 +75,7 @@ cmd({
 
     await conn.sendMessage(m.chat, {
       image: imageBuffer,
-      caption: `Background removed\n\n> *𝐀𝐍𝐀𝐘𝐀𝐓-𝐀𝐈*`
+      caption: `Background removed\n\n> *𝙵𝚊𝚒𝚣𝚊𝚗-𝙰𝚒*`
     });
 
   } catch (error) {
